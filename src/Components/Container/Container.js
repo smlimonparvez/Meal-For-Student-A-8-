4e7eca7fea29ceab5d7898664c1e0c5a09f8d823
smlimonparvez <smlimonparvez@gmail.com>
2022-09-30 09:02:37 +0600

    eightth

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -4,15 +4,19 @@ import Card from '../Card/Card';
 import './Container.css';
 
 const Container = () => {
-    const [cards, setCards] =  useState([]);
-    useEffect(() =>{
-        fetch('card.json')
-        .then(res => res.json())
-        .then(data => setCards(data))
-    },[])
+    const [cards, setCards] = useState([]);
+    const [time, setTime] = useState([]);
 
-    const handleAddToInput =(card)=>{
-console.log('click')
+    useEffect(() => {
+        fetch('card.json')
+            .then(res => res.json())
+            .then(data => setCards(data))
+    }, [])
+
+    const handleAddToInput = (card) => {
+        console.log('click');
+        const newTime = [...time, card];
+        setTime(newTime);
     }
 
     return (
@@ -20,14 +24,14 @@ console.log('click')
             <div className='card-item'>
                 <h2>MEAL FOR STUDENT</h2>
                 <h3>Today's Meal Chart:</h3>
-               <div className='card-container'>
-               {
-                    cards.map(card => <Card key={card.id}
-                        card = {card}
-                        handleAddToInput = {handleAddToInput}
-                    ></Card>)
-                }
-               </div>
+                <div className='card-container'>
+                    {
+                        cards.map(card => <Card key={card.id}
+                            card={card}
+                            handleAddToInput={handleAddToInput}
+                        ></Card>)
+                    }
+                </div>
             </div>
             <div className='aside-bar'>
                 <Asidebar></Asidebar>
